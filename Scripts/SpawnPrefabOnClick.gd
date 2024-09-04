9acfbb4a91c52ca1cb3a7ec973de2190fe994841
BoomBoomMushroom <77977795+BoomBoomMushroom@users.noreply.github.com>
2024-09-04 11:41:40 -0400

    mouse not clicking weird hitbox

--- Scripts/SpawnPrefabOnClick.gd
+++ Scripts/SpawnPrefabOnClick.gd
@@ -2,20 +2,25 @@ extends Node2D
 
 @onready var globals_manager = %GlobalsManager
 
+var collisionShape: CollisionShape2D
+var rect: Rect2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	collisionShape = get_child(0)
+	rect = collisionShape.shape.get_rect()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.button_index == MOUSE_BUTTON_LEFT:
-		get_viewport_rect()
+		#get_viewport_rect()
 		
-		if get_viewport_rect().has_point(event.position):
+		print(rect, event.position, rect.has_point(event.position))
+		if rect.has_point(event.position):
 			print('test')
 			globals_manager.blockMouseFromMovingCamera = true
-
